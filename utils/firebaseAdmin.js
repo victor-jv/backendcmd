9b0f25d39de27fb47b9f2b8d75432e4614f0ad6c
João Victor <112131417+victor-jv@users.noreply.github.com>
2025-06-19 21:59:54 -0300

    Update firebaseAdmin.js

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -2,9 +2,11 @@ const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// Inicializar diretamente pelo caminho da chave
+// Carrega as credenciais do Firebase a partir de uma variável de ambiente JSON stringificada
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
 admin.initializeApp({
-  credential: admin.credential.cert(require(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
